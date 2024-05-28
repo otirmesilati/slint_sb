81838175dd0f0b2dd0ad0761c0c25b161bf9fae2
Otir <otirmesi@gmail.com>
2024-05-28 18:39:09 +0300

    Rusting

--- incomeapp/src/main.rs
+++ incomeapp/src/main.rs
@@ -1,15 +1,25 @@
+use slint::SharedString;
 slint::include_modules!();
+
+slint::include_modules!();
+
+const TAXPER: f64 = 0.30;
+const OWNERPER: f64 = 0.55;
+const PROFITPER: f64 = 0.05;
+const OPEXPER: f64 = 0.10;
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
-
     let ui_handle = ui.as_weak();
-    // ui.on_request_increase_value({
-    //    move || {
-    //       let ui = ui_handle.unwrap();
-    //        ui.set_counter(ui.get_counter() + 1);
-    //   }
-    // });
+    ui.on_divide_income(move |string: SharedString|{
+    let ui: AppWindow = ui_handle.unwrap();
+    let num: f64 = string.trim().parse().unwrap();
+    let tax: f64 = num * TAXPER;
+    let owner: f64 = num * OWNERPER;
+    let profit: f64 = num * PROFITPER;
+    let opex: f64 = num * OPEXPER;
+    ui.set_counter(ui.get_counter() + 1);
+    });
 
     ui.run()
 }
