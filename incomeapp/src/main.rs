370615cfbb4b73430407cf3970573b4a79c86bfe
Otir <otirmesi@gmail.com>
2024-06-02 11:04:32 +0300

    slinting

--- incomeapp/src/main.rs
+++ incomeapp/src/main.rs
@@ -16,7 +16,7 @@ fn main() -> Result<(), slint::PlatformError> {
     let owner: f64 = num * OWNERPER;
     let profit: f64 = num * PROFITPER;
     let opex: f64 = num * OPEXPER;
-    let result: String = format!("Taxes: {:2}\nOwner: {:2}\nProfit: {:2}\nOpex: {:2}", tax, owner, profit, opex);
+    let result: String = format!("Taxes: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOpex: {:.2}", tax, owner, profit, opex);
     ui.set_results(result.into());
     });
 
